e7fc2ac4ea68e552144585523b7972136cc5f490
RadyaHarbani <radya.i4m@gmail.com>
2024-06-04 03:55:48 +0700

    feat : [RH] fetch API Laporan Harian Ananda dan Finishing Page Laporan

--- lib/app/pages/laporan-page/laporan_page_controller.dart
+++ lib/app/pages/laporan-page/laporan_page_controller.dart
@@ -4,25 +4,36 @@ import 'package:fun_education_app/app/api/alur-belajar/service/show_current_alur
 import 'package:fun_education_app/app/api/laporan-bulanan/models/show_current_laporan_bulanan_model.dart';
 import 'package:fun_education_app/app/api/laporan-bulanan/models/show_current_laporan_bulanan_response.dart';
 import 'package:fun_education_app/app/api/laporan-bulanan/service/show_current_laporan_bulanan_service.dart';
+import 'package:fun_education_app/app/api/laporan-harian/models/show_current_laporan_harian_model.dart';
+import 'package:fun_education_app/app/api/laporan-harian/models/show_current_laporan_harian_response.dart';
+import 'package:fun_education_app/app/api/laporan-harian/service/show_current_laporan_harian_service.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class LaporanPageController extends GetxController {
   RxBool isLoading = false.obs;
+  RxBool isLoadingLaporanHarian = false.obs;
 
   AlurBelajarService alurBelajarService = AlurBelajarService();
   ShowCurrentAlurBelajarResponse? showCurrentAlurBelajarResponse;
   Rx<ShowCurrentAlurBelajarModel> showCurrentAlurBelajarModel =
       ShowCurrentAlurBelajarModel().obs;
 
-  LaporanBulananService laporanHarianService = LaporanBulananService();
+  LaporanBulananService laporanBulananService = LaporanBulananService();
   ShowCurrentLaporanBulananResponse? showCurrentLaporanBulananResponse;
   Rx<ShowCurrentLaporanBulananModel> showCurrentLaporanBulananModel =
       ShowCurrentLaporanBulananModel().obs;
+
+  LaporanHarianService laporanHarianService = LaporanHarianService();
+  ShowCurrentLaporanHarianResponse? showCurrentLaporanHarianResponse;
+  Rx<ShowCurrentLaporanHarianModel> showCurrentLaporanHarianModel =
+      ShowCurrentLaporanHarianModel().obs;
 
   @override
   void onInit() {
     showCurrentAlurBelajar();
     showCurrentLaporanBulanan();
+    showCurrentLaporanHarian();
     super.onInit();
   }
 
@@ -42,8 +53,9 @@ class LaporanPageController extends GetxController {
   showCurrentLaporanBulanan() async {
     try {
       isLoading(true);
+      showCurrentLaporanBulananModel.value = ShowCurrentLaporanBulananModel();
       await Future.delayed(Duration.zero);
-      final response = await laporanHarianService
+      final response = await laporanBulananService
           .getShowCurrentLaporanBulanan(DateTime.now().month);
       showCurrentLaporanBulananResponse =
           ShowCurrentLaporanBulananResponse.fromJson(response.data);
@@ -55,6 +67,28 @@ class LaporanPageController extends GetxController {
       print(e);
     } finally {
       isLoading(false);
+    }
+  }
+
+  showCurrentLaporanHarian() async {
+    String formattedDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
+    DateTime parsedDate = DateTime.parse(formattedDate);
+    try {
+      isLoadingLaporanHarian(true);
+      showCurrentLaporanHarianModel.value = ShowCurrentLaporanHarianModel();
+      final response =
+          await laporanHarianService.getShowCurrentLaporanHarian(parsedDate);
+      showCurrentLaporanHarianResponse =
+          ShowCurrentLaporanHarianResponse.fromJson(response.data);
+      showCurrentLaporanHarianModel.value =
+          showCurrentLaporanHarianResponse!.data;
+
+      update();
+    } catch (e) {
+      isLoadingLaporanHarian(true);
+      print(e);
+    } finally {
+      isLoadingLaporanHarian(false);
     }
   }
 }
