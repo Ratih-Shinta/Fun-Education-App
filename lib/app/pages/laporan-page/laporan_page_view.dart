9460ca19dc7566d29231e8049d4165b03b3fd75b
ratih shinta <rstihshintaaa6@gmail.com>
2024-07-09 11:23:15 +0700

    slicing [RS] : tabbar tugas laporan

--- lib/app/pages/laporan-page/laporan_page_view.dart
+++ lib/app/pages/laporan-page/laporan_page_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:fun_education_app/app/pages/laporan-page/components/laporan_component.dart';
 import 'package:fun_education_app/app/pages/laporan-page/components/peringkat_component.dart';
 import 'package:fun_education_app/app/pages/laporan-page/laporan_page_controller.dart';
-import 'package:fun_education_app/app/pages/laporan-page/widgets/tab_button.dart';
 import 'package:fun_education_app/common/helper/themes.dart';
 import 'package:get/get.dart';
 
@@ -16,62 +15,65 @@ class LaporanPageView extends GetView<LaporanPageController> {
     final double width = mediaQuery.width;
     final double height = mediaQuery.height;
 
-    return Scaffold(
-        backgroundColor: backgroundColor,
-        appBar: AppBar(
-          backgroundColor: transparentColor,
-          title: AutoSizeText.rich(
-            TextSpan(
-              text: 'Laporan Ananda',
-              style: tsTitleSmallSemibold(blackColor),
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          backgroundColor: backgroundColor,
+          appBar: AppBar(
+            backgroundColor: transparentColor,
+            title: AutoSizeText.rich(
+              TextSpan(
+                text: 'Laporan Ananda',
+                style: tsTitleSmallSemibold(blackColor),
+              ),
+            ),
+            bottom: PreferredSize(
+              preferredSize: const Size.fromHeight(55),
+              child: ClipRRect(
+                child: Container(
+                  padding: EdgeInsets.all(5),
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    color: opacity5GreyColor,
+                  ),
+                  child: TabBar(
+                    // controller: controller.tabControllerAll,
+                    labelStyle: tsBodySmallSemibold(blackColor),
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    dividerColor: Colors.transparent,
+                    indicator: BoxDecoration(
+                      color: whiteColor,
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                    ),
+                    labelColor: blackColor,
+                    unselectedLabelColor: greyColor,
+                    tabs: [
+                      Tab(
+                        text: 'Laporan',
+                      ),
+                      Tab(
+                        text: 'Peringkat',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
-        ),
-        body: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: SafeArea(
+          body: SafeArea(
               child: Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: width * 0.05,
-                    vertical: height * 0.01,
-                  ),
-                  child: GestureDetector(
-                    onHorizontalDragEnd: controller.onHorizontalDrag,
-                    child: Column(
-                      children: [
-                        Container(
-                          width: width,
-                          padding: EdgeInsets.all(5),
-                          margin: EdgeInsets.only(bottom: 20),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              color: opacity5GreyColor),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              TabButton(
-                                title: 'Laporan',
-                                index: 0,
-                              ),
-                              TabButton(
-                                title: 'Peringkat',
-                                index: 1,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Obx(() {
-                          return IndexedStack(
-                            index: controller.currentIndex.value,
-                            children: [
-                              LaporanComponent(),
-                              PeringkatComponent(),
-                            ],
-                          );
-                        }),
-                      ],
-                    ),
-                  )),
-            )));
+            padding: EdgeInsets.symmetric(
+                horizontal: width * 0.05, vertical: height * 0.01),
+            child: TabBarView(
+              children: [
+                LaporanComponent(),
+                PeringkatComponent(),
+              ],
+            ),
+          )),
+        ));
   }
 }
